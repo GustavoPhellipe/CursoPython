259b3ef705548c419b0c30bb8989760460bf572e
GustavoPhellipe <152019272+GustavoPhellipe@users.noreply.github.com>
2024-06-12 09:17:37 -0300

    Nova aula 012 de Python

---
+++ Aulas/aula012/aula12.py
@@ -0,0 +1,9 @@
+nome = str(input('Digite seu nome:'))
+if nome == 'Gustavo':
+          print('Olá, {}! Seu nome é bem bonito..'.format(nome))
+
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Eduardo':
+        print('Bom dia, {}! Seu nome é bem popular'.format(nome))
+
+else:
+        print('Olá, seja muito bem-vindo(a), {}!'.format(nome))
