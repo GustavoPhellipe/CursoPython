14bf7d36443407ff44cde4280778c6e05bd2b90a
GustavoPhellipe <152019272+GustavoPhellipe@users.noreply.github.com>
2024-06-30 22:28:51 -0300

    Novas aulas
    Curso Python

---
+++ Exercicios/ex038/ex038.py
@@ -0,0 +1,8 @@
+num = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+if num > num2:
+          print('O primeiro número é maior')
+elif num < num2:
+        print('O segundo número é maior')
+elif num == num2:
+        print('Os seguintes números são iguais')
