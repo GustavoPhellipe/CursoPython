d1d9e1c573a8f438775530954daabd7bbf9366be
Gustavo448 <152019272+GustavoPhellipe@users.noreply.github.com>
2024-07-24 15:40:51 -0300

    Atualização

--- Exercicios/ex058/ex058.py
+++ Exercicios/ex058/ex058.py
@@ -2,7 +2,7 @@ import random
 from time import sleep
 Computador = random.randint(1,11)
 cont = 0
-print('Computador: Olá! Tudo bem? Vamos jogar um novo jogo de Adivinhação?')
+print('Computador: Olá, tudo bem? Vamos jogar um novo jogo de Adivinhação?')
 sleep(4.5)
 print('Computador: Começarei a "pensar" em um número de 1 à 10, e preciso q vc tente adivinhar o número q eu escolhi')
 sleep(4.5)
@@ -12,5 +12,5 @@ num = int(input('Escolha um número:'))
 while num != Computador:
     num = int(input('Computador: Número Errado! Tente adivinhar o número que eu pensei:'))
     cont += 1
-print('PARABÉNS! Você conseguiu adivinhar o número {} que o computador pensou!'.format(Computador))
+print('PARABÉNS! Você conseguiu adivinhar o número {}!'.format(Computador))
 print('Você fez {} tentativa(s)'.format(cont))
