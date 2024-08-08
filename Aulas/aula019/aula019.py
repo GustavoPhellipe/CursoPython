f722444988be7ab8ee8db01a6c628909f6af30a0
GustavoPhellipe <152019272+GustavoPhellipe@users.noreply.github.com>
2024-08-08 11:10:14 -0300

    Nova aula 19

---
+++ Aulas/aula019/aula019.py
@@ -0,0 +1,8 @@
+dados = dict()
+dados = {'nome':'Gustavo','idade':16}
+print('O {} tem {} anos'.format(dados['nome'],dados['idade']))
+'''for k in dados:
+          print(k)
+for k, v in dados.items():
+        print('{} = {}'.format(k,v))
+'''
