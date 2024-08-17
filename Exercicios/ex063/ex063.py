9efa3ac8dbdc5cdfe1627befc3596ba4273b6808
GustavoPhellipe <152019272+GustavoPhellipe@users.noreply.github.com>
2024-08-17 12:13:19 -0300

    Pequenas atualizações

--- Exercicios/ex063/ex063.py
+++ Exercicios/ex063/ex063.py
@@ -1,2 +1,19 @@
-termo = int(input('Quantos termos você quer mostrar?'))
-cont = 0
+'''termo = int(input('Quantos termos você quer mostrar?'))
+cont = 0'''
+
+primeiro = int(input('Digite um número para o termo:'))
+razao = int(input('Digite um número para a razão:'))
+termo = primeiro
+contador = 1
+mais_termos = 10
+
+while mais_termos != 0:
+    total = 0
+    while total < mais_termos:
+        print(f'{termo}', end=' - ')
+        termo += razao
+        total += 1
+    print('Pausa')
+    mais_termos = int(input('Você quer adicionar quantos termos a mais?'))
+
+print('Fim da progressão aritmética')
--- Exercicios/ex093/ex093.py
+++ Exercicios/ex093/ex093.py
@@ -1,16 +1,21 @@
-'''Futebol = dict()
-Gols = list()
-cont = 0
-Futebol['jogador'] = str(input('Digite o nome de um jogador:')).strip()
-for n in range(1,5):
-          Gol = int(input('Quantos gols o jogador {} marcou na partida {}?'.format(Futebol['jogador'], n)))
-          Gols.append(Gol)
-          cont = cont + Gol
-print('-=-' * 14)
-for k, v in enumerate(Gols):
- print(f'{k+1}º Partida: {Gols[k]}')
-print('O jogador {} marcou no total {} gols.'.format(Futebol['jogador'],cont))
-print('-=-' * 14)'''
+'''jogador = dict()
+partidas = list()
+jogador['nome'] = str(input('Nome do jogador:')).strip()
+tot = int(input('Quantas partidas ele jogou?'))
+for c in range (0,tot):
+          partidas.append( int(input(f'Quantos gols na partida {c}?')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-=-' * 30)
+print(jogador)
+print('-=-' * 30)
+for k, v in jogador.items():
+        print(f'O campo {k} tem o valor {v}.')
+print('-=-' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+for i, v in enumerate(jogador['gols']):
+        print(f'   => Na partida {i}, fez {v} gols')
+print(f'O total de gols foram {jogador["total"]}')'''
 
 Futebol = dict()
 Gols = list()
